933fe50865a679dce40f0cdbaf6d2bf0b72ca836
JustRollingDevs <justrollingdevs@protonmail.com>
2023-04-16 13:56:04 +0200

    added guards for Order.routes

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+// Controllers
 import OrderController from '../controllers/order.controller';
+// middlewares
+import { jwtToken, guard } from '../middlewares/auth.middleware';
+// Types
 import { IRoute } from '../interfaces/route.interface';
 
 class OrderRoute implements IRoute {
@@ -11,12 +15,53 @@ class OrderRoute implements IRoute {
   }
 
   private initializeRoutes() {
-    this.router.get('/orders', this.controller.index);
-    this.router.get('/orders/baker/:baker', this.controller.getBakerOrders);
-    this.router.get('/order/:id', this.controller.indexOne);
-    this.router.post('/order', this.controller.createOrder);
-    this.router.put('/order/:id', this.controller.editOrder);
-    this.router.delete('/order/:id', this.controller.remove);
+    this.router.get(
+      '/orders',
+      [jwtToken(true), guard.check('admin')],
+      this.controller.index
+    );
+    this.router.get(
+      '/orders/baker/:baker',
+      [jwtToken(true), guard.check([['admin'], ['baker:read', 'baker:write']])],
+      this.controller.getBakerOrders
+    );
+    this.router.get(
+      '/order/:id',
+      [
+        jwtToken(true),
+        guard.check([
+          ['admin'],
+          ['member:read', 'member:write'],
+          ['baker:read', 'baker:write'],
+        ]),
+      ],
+      this.controller.indexOne
+    );
+    this.router.post('/order', [jwtToken()], this.controller.createOrder);
+    this.router.put(
+      '/order/:id',
+      [
+        jwtToken(true),
+        guard.check([
+          ['admin'],
+          ['member:read', 'member:write'],
+          ['baker:read', 'baker:write'],
+        ]),
+      ],
+      this.controller.editOrder
+    );
+    this.router.delete(
+      '/order/:id',
+      [
+        jwtToken(true),
+        guard.check([
+          ['admin'],
+          ['member:read', 'member:write'],
+          ['baker:read', 'baker:write'],
+        ]),
+      ],
+      this.controller.remove
+    );
   }
 }
 
